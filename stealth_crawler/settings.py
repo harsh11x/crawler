e6e49b74e8001e0902204f4d7951c6452c6ffcbc
Harsh <63919951+harsh11x@users.noreply.github.com>
2024-10-12 20:49:35 +0530

    Update settings.py

--- stealth_crawler/settings.py
+++ stealth_crawler/settings.py
@@ -1,129 +1,58 @@
 # Scrapy settings for stealth_crawler project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = "stealth_crawler"
 
 SPIDER_MODULES = ["stealth_crawler.spiders"]
 NEWSPIDER_MODULE = "stealth_crawler.spiders"
 
-
+# User-Agent configuration
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "stealth_crawler (+http://www.yourdomain.com)"
+# Uncomment and update the user-agent as needed
+# USER_AGENT = "stealth_crawler (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+DOWNLOAD_DELAY = 2
+RANDOMIZE_DOWNLOAD_DELAY = True
 
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# Enable AutoThrottle to automatically slow down based on site load
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1  # Initial download delay
+AUTOTHROTTLE_MAX_DELAY = 5  # Max download delay
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0  # Average number of requests per second per remote server
 
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# Disable cookies to reduce tracking
+COOKIES_ENABLED = False
 
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-#}
+# Configure downloader middlewares
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,  # Disable default UserAgentMiddleware
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,  # Use Random User-Agent middleware
+    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,  # Enable HTTP Proxy middleware
+    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,  # Enable rotating proxies
+    'rotating_proxies.middlewares.BanDetectionMiddleware': 620,  # Enable ban detection
+}
 
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "stealth_crawler.middlewares.StealthCrawlerSpiderMiddleware": 543,
-#}
+# Proxy configuration
+HTTP_PROXY = 'http://127.0.0.1:8118'  # Tor default HTTP proxy
 
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "stealth_crawler.middlewares.StealthCrawlerDownloaderMiddleware": 543,
-#}
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
-
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "stealth_crawler.pipelines.StealthCrawlerPipeline": 300,
-#}
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = "httpcache"
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+# Uncomment if you have a custom ProxyMiddleware implemented
+# 'stealth_crawler.middlewares.ProxyMiddleware': 100,
 
 # Set settings whose default value is deprecated to a future-proof value
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 
-# Randomize the user-agent for each request
-DOWNLOADER_MIDDLEWARES = {
-    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
-}
-
-
-# Add a delay between requests (e.g., 1-3 seconds)
-DOWNLOAD_DELAY = 2
-RANDOMIZE_DOWNLOAD_DELAY = True
-
-# Enable AutoThrottle to automatically slow down based on site load
-AUTOTHROTTLE_ENABLED = True
-AUTOTHROTTLE_START_DELAY = 1
-AUTOTHROTTLE_MAX_DELAY = 5
-
-
-# In settings.py
-PROXY_POOL_ENABLED = True
-DOWNLOADER_MIDDLEWARES.update({
-    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 1,
-    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
-    'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
-})
-
-# Disable cookies to reduce tracking
-COOKIES_ENABLED = False
-
-
-# In settings.py
-HTTP_PROXY = 'http://127.0.0.1:8118'  # Tor default HTTP proxy
-DOWNLOADER_MIDDLEWARES = {
-    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
-    'my_project.middlewares.ProxyMiddleware': 100,
-}
+# Enable HTTP caching (disabled by default)
+# Uncomment to enable and configure HTTP caching
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = "httpcache"
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
